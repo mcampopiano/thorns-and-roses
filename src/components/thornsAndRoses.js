2abb8491b892524d6e19185b652c5b68143b9f09
Mario Campopiano <mcampopiano92@gmail.com>
2020-12-02 12:05:01 -0600

    started building boilerplate info

---
+++ src/components/thornsAndRoses.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { Route } from "react-router-dom"
+
+
+export const ThornsAndRoses = () => (
+    <>
+        <Route exact path="/">
+            <NavBar />
+            
+        </Route>
+    </>
+)
