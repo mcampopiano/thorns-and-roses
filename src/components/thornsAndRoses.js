463cd0c8d5b69b05e1c6afe532cf7c806ef64817
Mario Campopiano <mcampopiano92@gmail.com>
2020-12-02 14:31:59 -0600

    Added a navigation bar and routes

--- src/components/thornsAndRoses.js
+++ src/components/thornsAndRoses.js
@@ -1,12 +1,12 @@
 import React from "react"
 import { Route } from "react-router-dom"
+import { NavBar } from "./nav/NavBar"
 
 
 export const ThornsAndRoses = () => (
     <>
         <Route exact path="/">
-            <NavBar />
-            
+            <NavBar />            
         </Route>
     </>
 )
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ThornsAndRoses } from './components/thornsAndRoses';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThornsAndRoses />
+    <Router>
+      <ThornsAndRoses />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
